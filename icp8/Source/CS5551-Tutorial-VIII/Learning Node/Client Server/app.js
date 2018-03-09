a2b1af9382c88ea09f08167f017dfff91a2949ff
sravyapara <35548441+sravyapara@users.noreply.github.com>
2018-03-08 22:23:38 -0600

    commit1

---
+++ icp8/Source/CS5551-Tutorial-VIII/Learning Node/Client Server/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+            var searchQuery = document.getElementById("txt_searchFilter").value;
+            if (placeEntered != null && placeEntered != "" && searchQuery != null && searchQuery != "") {
+
+                //This is the API that gives the list of venues based on the place and search query.
+                var handler = $http.get("http://numbersapi.com/"+placeEntered+"/"+searchQuery+"/date?json");
+                handler.success(function (data) {
+
+                            $scope.venueList[0] = {
+                                "name": data.text
+                            };
+
+                })
+                handler.error(function (data) {
+                    alert("There was some error processing your request. Please try after some time.");
+                });
+            }
+        }
+
+
+    });
